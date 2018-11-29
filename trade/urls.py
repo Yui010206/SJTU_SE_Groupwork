ac55de9c034bd2772c21d044caaa1dc7825ef345
517030910244 <43175660+517030910244@users.noreply.github.com>
2018-11-29 22:46:46 +0800

    Add files via upload

--- trade/urls.py
+++ trade/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('trade.views',
-        url(r'^goodsList', 'goodsList'),
         url(r'^goodsPurchase', 'goodsPurchase'),
         url(r'^buyHistory', 'buyHistory'),
         url(r'^saleHistory','saleHistory'),
         url(r'^confirmation','confirmation'),
+        url(r'^rerelease','rerelease'),
         url(r'(?P<good_id>\d)/$','detail')
 )
