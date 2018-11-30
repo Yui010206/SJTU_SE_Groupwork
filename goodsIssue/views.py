5fad823627ec7f48120717efb6aed6c754607cac
Yui010206 <38401999+Yui010206@users.noreply.github.com>
2018-11-30 23:15:55 +0800

    Add files via upload

--- goodsIssue/views.py
+++ goodsIssue/views.py
@@ -1,13 +1,11 @@
 #encoding: utf-8
 from django.shortcuts import render
-
 from models import GoodsReleased,GoodsReleaseForm
 from account.models import LoginUser
 from dtiaozao import function as fun
 import PIL
 
 #新品发布
-
 def release(request):
     if not request.session.get('islogin'):
         msg = '你还未登陆，请先登陆！'
@@ -21,9 +19,8 @@ def release(request):
             cd  = releaseform.cleaned_data
             uid = request.session['user_info']['uid'] # 获取用户ID
             new_good = releaseform.save(commit = False)
-
+            new_good.saler_id = uid
             new_good.status = 1
-
             new_good.save()
             msg = "商品发布成功！"
             return render(request,"error_msg.html",locals())
@@ -32,16 +29,12 @@ def release(request):
             return render(request,"error_msg.html",locals())
 
 
-
 #浏览市场
-
 def market(request):
         allgoods = GoodsReleased.objects.filter(status=1)
         return render(request, "Good_Market.html",{"allgoods":allgoods},)
         
-
 #查看详情
-
 def detail(request,good_id):
     if not request.session.get('islogin'):
         msg = "你还未登陆，请先登陆！"
@@ -61,7 +54,6 @@ def search(request):
         goods_name = data['keywords']
         goods_info = GoodsReleased.objects.filter(title__contains = goods_name)
         return render(request,'Good_Searched.html',locals(),) 
-
 
 
 
